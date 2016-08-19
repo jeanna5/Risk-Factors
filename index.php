93854b6e62c93802e3955d3723866f40d3e9df1e
Jeanna Gindi <jagindi@gmail.com>
2016-08-19 10:39:31 -0700

    minor updates to php file

--- index.php
+++ index.php
@@ -3,30 +3,44 @@
   <title>Risk Factors</title>
  </head>
  <body>
- <?php echo '<p>Risk Factors</p>'; 
+ <?php echo '<h1>Risk Factors</h1>';
 
 
 $dbc = @mysqli_connect('localhost', 'root', 'password', 'riskfactors') OR die('could not connect to mysql: ' . mysqli_connect_error());
 
-$query = "SELECT vw.user_id, vw.inbox_valid_word_percent, vw.outbox_valid_word_percent, sc.shared_phone_numbers FROM valid_words_tbl vw JOIN shared_contact_tbl sc ON vw.user_id = sc.user_id ORDER BY user_id";
+$query = "SELECT vw.user_id, vw.inbox_valid_word_percent, vw.outbox_valid_word_percent, vw.inbox_total_words, vw.outbox_total_words, vw.inbox_total_messages, vw.outbox_total_messages, sc.shared_phone_numbers FROM valid_words_tbl vw JOIN shared_contact_tbl sc ON vw.user_id = sc.user_id ORDER BY user_id";
 
 $response = @mysqli_query($dbc, $query);
 
 if($response){
-	echo "<table border='1',style='width:100%'>
-	<tr>
-	<th align='left'>User</th>
-	<th>Valid Words in Inbox</th>
-	<th>Valid Words in Outbox</th>
-	<th>Phone Numbers Shared With Other Users</th>	
-	</tr>";
+	?>
 
+	<table border="1",style="width:100%">
+	<tr>
+	<th align="left">User</th>
+	<th>Valid Words in Inbox</th>
+	<th>Inbox Total Messages</th>
+	<th>Inbox Total Words</th>
+
+	<th>Valid Words in Outbox</th>
+	<th>Outbox Total Messages</th>
+	<th>Outbox Total Words</th>
+	<th>Phone Numbers Shared With Other Users</th>
+	</tr>
+
+	<?php
 	while($row = mysqli_fetch_array($response)){
 		echo "<tr>";
 		echo "<td>" .$row['user_id']."</td>";
-		echo "<td align='center'>".$row['inbox_valid_word_percent']."%</td>";
-		echo "<td align='center'>".$row['outbox_valid_word_percent']."%</td>";
-		echo "<td align='center'>".$row['shared_phone_numbers']."</td>";
+		echo "<td bgcolor = \"#e6fff2\", align=\"center\">".$row['inbox_valid_word_percent']."%</td>";
+		echo "<td bgcolor = \"#e6fff2\", align=\"center\">".$row['inbox_total_messages']."</td>";
+		echo "<td bgcolor = \"#e6fff2\", align=\"center\">".$row['inbox_total_words']."</td>";
+
+		echo "<td bgcolor = \"#e6ffff\", align=\"center\">".$row['outbox_valid_word_percent']."%</td>";
+		echo "<td bgcolor = \"#e6ffff\", align=\"center\">".$row['outbox_total_messages']."</td>";
+		echo "<td bgcolor = \"#e6ffff\", align=\"center\">".$row['outbox_total_words']."</td>";
+
+		echo "<td bgcolor = \"#ffffe6\", align=\"center\">".$row['shared_phone_numbers']."</td>";
 		echo "</tr>";
 	}
 	echo '</table>';
@@ -38,6 +52,6 @@ else {
 
 mysqli_close($dbc);
 
-?> 
+?>
  </body>
 </html>
